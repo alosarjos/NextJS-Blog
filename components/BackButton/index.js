ee9709b815a46fd592a420a2e9eea8668c5cc3a4
José Miguel Sarasola <alosarjos@gmail.com>
2021-12-18 11:51:40 +0100

    Update site styles

--- components/BackButton/index.js
+++ components/BackButton/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function BackButton({ href }) {
   return (
     <Link href={href} passHref>
-      <button className="absolute left-6 top-6 px-4 py-2 border-2 border-gray-300 rounded-lg transition duration-300  hover:border-gray-500 hover:shadow-lg">
+      <button className="absolute left-6 top-6 px-4 py-2 border-2 border-gray-300 rounded-lg transition duration-300 hover:border-gray-700 hover:shadow-lg">
         Go back
       </button>
     </Link>
